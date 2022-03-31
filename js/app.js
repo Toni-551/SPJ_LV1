feed2a83fd728d5fea07abc1383a9a303077b815
Toni Crnković <toni.crnkovic5@gmail.com>
2022-03-31 10:40:39 +0200

    Third commit

--- js/app.js
+++ js/app.js
@@ -5,11 +5,23 @@ function Press(){
     var semestar = document.getElementById("semestar").value;
     var ects = document.getElementById("ects").value;
     var table = document.getElementById("table");
-    var string = "<tr><td>"+naziv+"</td><td>"+semestar+"</td><td>"+ ects+"</td><td onclick='DeleteRow()'><button>d</button></td></tr>"
+    var string = "<tr><td>"+naziv+"</td><td>"+semestar+"</td><td>"+ ects+"</td><td onclick='DeleteRow()'><button>d</button></td><td><input onclick='CheckPressCheckbox()' type='checkbox'></td></tr>"
     table.innerHTML += string;
 }
 function DeleteRow(){
     var td = event.target.parentNode;
     var tr = td.parentNode;
     tr.parentNode.removeChild(tr);
+}
+
+function CheckPressCheckbox(){
+    var checkbox = event.target.checked;
+    var td = event.target.parentNode;
+    var tr = td.parentNode;
+
+    if(checkbox == true){
+        tr.style.backgroundColor = "Green";
+    }else{
+        tr.style.backgroundColor = "transparent";
+    }
 }
